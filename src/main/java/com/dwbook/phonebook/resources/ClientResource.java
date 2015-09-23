c5e46a210d4b4f0202c69b9a8915ceb8dd99812b
Robert Chen <robert888tw@hotmail.com>
2015-09-23 17:50:45 +0800

    Web service client (2) @Path("createContact")

--- src/main/java/com/dwbook/phonebook/resources/ClientResource.java
+++ src/main/java/com/dwbook/phonebook/resources/ClientResource.java
@@ -5,8 +5,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.dwbook.phonebook.representations.Contact;
 
@@ -30,6 +32,27 @@ public class ClientResource {
 				+ "\nPhone: " + c.getPhone();
 		return output;
 	}
+	
+	@GET
+	@Path("createContact")
+	public Response newContact(@QueryParam("firstName") String firstName,
+			@QueryParam("lastName") String lastName, @QueryParam("phone") String phone) {
+		WebTarget contactResource = client.target("http://localhost:8080/contact/");
+		Response response = contactResource.request(MediaType.APPLICATION_JSON)
+				.post(Entity.entity(new Contact(0, firstName, lastName, phone), 
+						MediaType.APPLICATION_JSON), Response.class);
+		if(response.getStatus() == 201) {
+			return Response
+					.status(302)
+					.entity("The contact was created successfully! The new contact can be found at " +
+					response.getHeaders().getFirst("Location")).build();
+		}
+		else {
+			return Response.status(422).entity(response.getEntity()).build();
+		}
+	
+	}
+	
 	
 	
 }
